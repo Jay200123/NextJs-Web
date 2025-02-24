7750cb7aa9948b3b18f4093d3233d53b34b0cc47
Jay200123 <kenken.ayonon@gmail.com>
2025-02-24 22:40:26 +0800

    Integrate posts API into zustand store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import { Store } from "@/types";
-import { createUsersApi } from "./api/users";
+import { createUsersApi } from "./api/user";
+import { createPostsApi } from "./api/post";
 
 export const useStore = create<Store>((...a) => ({
   ...createUsersApi(...a),
+  ...createPostsApi(...a),
 }));
