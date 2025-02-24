372f68cfd8b9373034b508e9eb7b482a5c35da59
Jay200123 <kenken.ayonon@gmail.com>
2025-02-24 22:17:45 +0800

    Add not-found page for handling routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div className="flex items-center justify-center">
+      <h3 className="text-lg font-medium md:text-3xl md:font-bold">
+        <span className="text-2xl md:text-4xl text-red-500 py-1 px-1">404</span>
+        Route Not Found
+      </h3>
+    </div>
+  );
+}
