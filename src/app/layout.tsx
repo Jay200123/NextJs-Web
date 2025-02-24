d05bed1eb1b70ee26f1678e45b2ed5188735599f
Jay200123 <kenken.ayonon@gmail.com>
2025-02-24 22:17:26 +0800

    Setup Parent Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
+import { Navbar, Footer } from "@/components";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <main className="min-h-screen w-full flex flex-col justify-between items-center">
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
