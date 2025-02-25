a6ee95672bd92427b5485c89bb267f5b11f9445f
Jay200123 <kenken.ayonon@gmail.com>
2025-02-25 16:45:09 +0800

    Add Todo API integration to store for state management

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,8 +2,10 @@ import { create } from "zustand";
 import { Store } from "@/types";
 import { createUsersApi } from "./api/user";
 import { createPostsApi } from "./api/post";
+import { createTodoApi } from "./api/todos";
 
 export const useStore = create<Store>((...a) => ({
   ...createUsersApi(...a),
   ...createPostsApi(...a),
+  ...createTodoApi(...a),
 }));
