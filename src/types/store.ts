9bbac782289d64d932084e681ea93d14e370ea59
Jay200123 <kenken.ayonon@gmail.com>
2025-02-25 17:00:02 +0800

    Add AlbumApiSlice to Store type definition

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,5 +1,6 @@
 import { UserApiSlice } from "./users";
 import { PostApiSlice } from "./post";
 import { TodoApiSlice } from "./todos";
+import { AlbumApiSlice } from "./album";
 
-export type Store = UserApiSlice & PostApiSlice & TodoApiSlice;
+export type Store = UserApiSlice & PostApiSlice & TodoApiSlice & AlbumApiSlice;
