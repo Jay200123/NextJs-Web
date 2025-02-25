b60e9aa2e7e100adeca30fdaafb1eef33a0ab8d5
Jay200123 <kenken.ayonon@gmail.com>
2025-02-25 20:17:48 +0800

    Enhance user API state management with user retrieval and addition functionality

--- src/state/api/user.ts
+++ src/state/api/user.ts
@@ -2,21 +2,35 @@ import { StateCreator } from "zustand";
 import { UserApiSlice } from "@/types";
 import axios from "axios";
 
-export const createUsersApi: StateCreator<UserApiSlice> = (_set) => ({
+export const createUsersApi: StateCreator<UserApiSlice> = (set) => ({
   users: [],
-  getAllUsers: async() => {
-    // fetch all users  
+  user: null,
+  getAllUsers: async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    set({ users: res?.data });
+    return res?.data;
   },
-  getUserById: async(id) => {
-    // fetch user by id
+  getUserById: async (id) => {
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    set({ user: res?.data });
+    return res?.data;
   },
-  addUser: async(user) => {
-    // add user
+
+  addUser: async (user) => {
+    const res = await axios.post(
+      "https://jsonplaceholder.typicode.com/users",
+      user
+    );
+
+    console.log(res);
+    set((state) => ({ users: [...state.users, res?.data] }));
   },
-  deleteUser: async(id) => {
+  deleteUser: async (id) => {
     // delete user
   },
-  updateUser: async(user) => {
+  updateUser: async (user) => {
     // update user
   },
 });
