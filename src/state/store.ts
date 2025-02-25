05231b4ef718a61c2ed2e8493ced066adc6fbc7c
Jay200123 <kenken.ayonon@gmail.com>
2025-02-25 16:59:37 +0800

    Add createAlbumApi to store for album management

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,9 +3,11 @@ import { Store } from "@/types";
 import { createUsersApi } from "./api/user";
 import { createPostsApi } from "./api/post";
 import { createTodoApi } from "./api/todos";
+import { createAlbumApi } from "./api/album";
 
 export const useStore = create<Store>((...a) => ({
   ...createUsersApi(...a),
   ...createPostsApi(...a),
   ...createTodoApi(...a),
+  ...createAlbumApi(...a),
 }));
