c0568c91d101c24827be18a596536107452e31d7
Jay200123 <kenken.ayonon@gmail.com>
2025-02-26 11:36:55 +0800

    Add delete functionality and improve user interface in users page

--- src/app/(api)/users/page.tsx
+++ src/app/(api)/users/page.tsx
@@ -5,28 +5,47 @@ import { useRouter } from "next/navigation";
 
 export default function () {
   const router = useRouter();
-  const { users, getAllUsers } = useStore();
+  const { users, user, getAllUsers, deleteUser } = useStore();
 
   useQuery({
     queryKey: ["users"],
     queryFn: getAllUsers,
+    enabled: false,
+    refetchOnWindowFocus: false, // Disable refetching when switching tabs
+    refetchOnReconnect: false, // Disable refetching when internet reconnects
+    refetchOnMount: false, // Prevent refetching when component remounts
   });
 
+  const handleDelete = async (id: number) => {  
+    if (window.confirm("Are you sure you want to delete this user?")) {
+      await deleteUser(id);
+    }
+  }
 
   return (
     <div className="flex items-center justify-center w-full mt-4 h-full">
       <div className="flex flex-col w-[32rem] items-center h-full border rounded-md border-gray-400">
         <div className="flex items-center w-full justify-between px-4 py-2">
-        <h1 className="text-3xl font-bold">Users</h1>
-        <h1 onClick={()=>router.push("/users/add")} className="text-3xl cursor-pointer font-bold">+</h1>
+          <h1 className="text-3xl font-bold">Users</h1>
+          <h1
+            onClick={() => router.push("/users/add")}
+            className="text-3xl cursor-pointer font-bold"
+          >
+            +
+          </h1>
         </div>
         {users.map((user) => (
           <div
             key={user.id}
-            onClick={() => router.push(`/users/${user.id}`)}  
             className="flex items-center justify-between p-4 cursor-pointer my-2 bg-gray-100 rounded-md w-[26rem]"
           >
             <div className="w-full flex flex-col text-left">
+            <h1
+              onClick={() => handleDelete(user.id)}
+              className="text-lg text-red-500 cursor-pointer font-bold"
+            >
+              Delete
+            </h1>
               <h1 className="text-lg font-bold">{user.username}</h1>
               <p className="text-sm">{user.email}</p>
               <h1 className="text-base font-medium">Address</h1>
@@ -37,6 +56,20 @@ export default function () {
               <p className="text-sm">
                 {user?.company?.name} <span>{user?.company?.catchPhrase}</span>
               </p>
+              <div className="flex items-center w-full justify-between px-4 py-2">
+                <h1
+                  onClick={() => router.push(`/users/${user.id}`)}
+                  className="text-lg font-bold"
+                >
+                  View User
+                </h1>
+                <h1
+                  onClick={() => router.push(`/users/edit/${user.id}`)}
+                  className="text-lg cursor-pointer font-bold"
+                >
+                  Edit
+                </h1>
+              </div>
             </div>
           </div>
         ))}
