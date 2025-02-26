5b78345a0ffcc1fc436eec055bedacfc3c98eb08
Jay200123 <kenken.ayonon@gmail.com>
2025-02-26 11:37:18 +0800

    Refactor user API interactions to use centralized API client and implement user update functionality

--- src/state/api/user.ts
+++ src/state/api/user.ts
@@ -1,35 +1,39 @@
 import { StateCreator } from "zustand";
-import { UserApiSlice, User } from "@/types";
-import axios from "axios";
+import { UserApiSlice } from "@/types";
+import api from "../apiClient";
 
 export const createUsersApi: StateCreator<UserApiSlice> = (set) => ({
   users: [],
   user: null,
   getAllUsers: async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    const res = await api.get("/users");
     set({ users: res?.data });
     return res?.data;
   },
   getUserById: async (id) => {
-    const res = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    set({ user: res?.data });
-    return res?.data;
+    set((state) => ({
+      user: state.users.find((u) => u.id === id), 
+    }));
   },
 
   addUser: async (payload) => {
-    const res = await axios.post(
-      "https://jsonplaceholder.typicode.com/users",
-      payload
-    );
+    const res = await api.post("/users", payload);
 
-    set((state) => ({ users: [...state.users, res?.data] })); 
+    set((state) => ({ users: [...state.users, res?.data] }));
   },
   deleteUser: async (id) => {
-    // delete user
+    await api.delete(`/users/${id}`);
+    set((state) => ({
+      users: state.users.filter((u) => u.id !== id),
+    }));
   },
-  updateUser: async (user) => {
-    // update user
+  updateUser: async (id, payload) => {
+    await api.put(`/users/${id}`);
+    set((state) => ({
+      users: state.users.map((u) => (u.id === id ? payload : u)),
+    }));
+  },
+  clearUser: () => {
+    set({ user: null });
   },
 });
