b9f908b27a917f98696d0b5eb1918d13b46b7d7c
Jay200123 <kenken.ayonon@gmail.com>
2025-02-26 20:43:14 +0800

    Update Todo page to correct references and improve delete functionality

--- src/app/(api)/todos/page.tsx
+++ src/app/(api)/todos/page.tsx
@@ -9,15 +9,14 @@ export default function () {
   useQuery({
     queryKey: ["todos"],
     queryFn: getAllTodos,
-    // enabled: false,
+    enabled: false,
     // refetchOnWindowFocus: false, // Disable refetching when switching tabs
     // refetchOnReconnect: false, // Disable refetching when internet reconnects
     // refetchOnMount: false, // Prevent refetching when component remounts
   });
-  // const postsData = useMemo(() => posts?.slice(0, 20), [posts]);
 
   const handleDelete = async (id: number) => {
-    if (window.confirm("Are you sure you want to delete this post?")) {
+    if (window.confirm("Are you sure you want to delete this todo?")) {
       await deleteTodo(id);
     }
   };
@@ -28,7 +27,7 @@ export default function () {
         <div className="flex items-center w-full justify-between px-4 py-2">
           <h1 className="text-3xl font-bold">Todo</h1>
           <h1
-            // onClick={() => router.push("/posts/add")}
+            onClick={() => router.push("/todos/add")}
             className="text-3xl cursor-pointer font-bold"
           >
             +
@@ -41,7 +40,7 @@ export default function () {
           >
             <div className="w-full flex flex-col text-left">
               <h1
-                // onClick={() => handleDelete(post.id)}
+                onClick={() => handleDelete(todo.id)}
                 className="text-lg text-red-500 cursor-pointer font-bold"
               >
                 Delete
@@ -52,13 +51,13 @@ export default function () {
               </p>
               <div className="flex items-center w-full justify-between px-4 py-2">
                 <h1
-                  //   onClick={() => router.push(`/posts/${post.id}`)}
+                  onClick={() => router.push(`/todos/${todo.id}`)}
                   className="text-lg font-bold"
                 >
                   View Todo
                 </h1>
                 <h1
-                  // onClick={() => router.push(`/posts/edit/${post.id}`)}
+                  onClick={() => router.push(`/todos/edit/${todo.id}`)}
                   className="text-lg cursor-pointer font-bold"
                 >
                   Edit
