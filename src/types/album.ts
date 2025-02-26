2c440eb9b15b2f4bd1affe84102b15ded80b08ba
Jay200123 <kenken.ayonon@gmail.com>
2025-02-26 20:57:31 +0800

    Refactor album types to include album state and enhance action signatures

--- src/types/album.ts
+++ src/types/album.ts
@@ -6,14 +6,16 @@ type Album = {
 
 type AlbumState = {
   albums: Album[];
+  album: Album | null;
 };
 
 type AlbumActions = {
   getAllAlbums: () => Promise<void>;
   getAlbumById: (id: number) => Promise<void>;
-  addAlbum: (album: Album) => Promise<void>;
-  updateAlbum: (album: Album) => Promise<void>;
+  addAlbum: (payload: Album) => Promise<void>;
+  updateAlbum: (id: number, payload: Album) => Promise<void>;
   deleteAlbum: (id: number) => Promise<void>;
+  clearAlbum: () => void; 
 };
 
 export type AlbumApiSlice = AlbumState & AlbumActions;
